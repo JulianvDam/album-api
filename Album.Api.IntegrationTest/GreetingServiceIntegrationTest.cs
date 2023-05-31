3912caa1e60938095a2bac0e974d6f3a0266bd3f
JulianvDam <1038219@hr.nl>
2023-05-31 09:56:52 +0200

    IntegrationTest

--- Album.Api.IntegrationTest/GreetingServiceIntegrationTest.cs
+++ Album.Api.IntegrationTest/GreetingServiceIntegrationTest.cs
@@ -1,59 +1,34 @@
-using Xunit;
-using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Net;
-using System.Text.Json;
-using System;
-
-
-namespace Album.Api.Tests
+using Xunit;
+using  Microsoft.AspNetCore.Mvc.Testing;
+namespace Album.Api.IntegrationTests
 {
     public class GreetingServiceIntegrationTest : IClassFixture<WebApplicationFactory<Program>>
     {
+        private readonly HttpClient _client;
         private readonly WebApplicationFactory<Program> _factory;
 
         public GreetingServiceIntegrationTest(WebApplicationFactory<Program> factory)
         {
+            _client = factory.CreateClient();
             _factory = factory;
+
         }
 
         [Fact]
-        public async Task GetGreeting_WithName_ReturnsHelloName()
+        public async Task GetHello_WithValidName_ReturnsHelloName()
         {
             // Arrange
-            var client = _factory.CreateClient();
-            var name = "John";
-            var expectedGreeting = $"Hello, {name}";
+            var expectedResponse = "{\"result\":\"Hello, John\"}";
 
             // Act
-            var response = await client.GetAsync($"/api/hello?name={name}");
-            var content = await response.Content.ReadAsStringAsync();
-            var greeting = JsonSerializer.Deserialize<HelloController.HelloResponse>(content)?.Result;
+            var response = await _client.GetAsync("/api/hello?name=John");
+            var responseContent = await response.Content.ReadAsStringAsync();
 
             // Assert
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            Assert.Equal(expectedGreeting, greeting);
-        }
-
-        [Theory]
-        [InlineData(null)]
-        [InlineData("")]
-        [InlineData("   ")]
-        public async Task GetGreeting_WithNullOrWhitespace_ReturnsHelloWorld(string name)
-        {
-            // Arrange
-            var client = _factory.CreateClient();
-            var expectedGreeting = "Hello, World";
-
-            // Act
-            var response = await client.GetAsync($"/api/hello?name={name}");
-            var content = await response.Content.ReadAsStringAsync();
-            var greeting = JsonSerializer.Deserialize<HelloController.HelloResponse>(content)?.Result;
-
-            // Assert
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            Assert.Equal(expectedGreeting, greeting);
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(expectedResponse, responseContent);
         }
     }
 }
