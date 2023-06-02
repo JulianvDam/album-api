3e3efa9926e4847eaf3c28bcb1550aef76f4a519
JulianvDam <1038219@hr.nl>
2023-06-02 10:47:28 +0200

    Lesson 4: Added Dns.GetHostName()

--- Album.Api/Services/GreetingService.cs
+++ Album.Api/Services/GreetingService.cs
@@ -1,3 +1,5 @@
+using System.Net;
+
 public class GreetingService
 {
     private readonly ILogger<GreetingService> _logger;
@@ -6,15 +8,17 @@ public class GreetingService
     {
         _logger = logger;
     }
+
     public string GetGreeting(string name)
     {
         //logs that the method has been called
         _logger.LogInformation($"GreetingService: GetGreeting method called with name: {name} at {DateTime.UtcNow}");
         var startTime = DateTime.UtcNow;
+        string hostname = Dns.GetHostName();
 
         if (string.IsNullOrWhiteSpace(name))
         {
-            var greeting = "Hello, World";
+            var greeting = $"Hello, World from {hostname}";
             var endTime = DateTime.UtcNow;
             _logger.LogInformation($"GreetingService: returning greeting: {greeting} at {DateTime.UtcNow}. Duration: {endTime-startTime}");
 
@@ -22,7 +26,7 @@ public class GreetingService
         }
         else
         {
-            var greeting = $"Hello, {name}";
+            var greeting = $"Hello, {name} from {hostname}";
             var endTime = DateTime.UtcNow;
             _logger.LogInformation($"GreetingService: returning greeting: {greeting} at {DateTime.UtcNow}. Duration: {endTime-startTime}");
 
